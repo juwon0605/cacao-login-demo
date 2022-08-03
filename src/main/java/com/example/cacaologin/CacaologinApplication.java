2bb0604f6c5137cfc62c51957e55b76411bf118e
Juwon Park <devprofessionalism@gmail.com>
2022-08-03 16:47:08 +0900

    :tada: 프로젝트 생성

---
+++ src/main/java/com/example/cacaologin/CacaologinApplication.java
@@ -0,0 +1,13 @@
+package com.example.cacaologin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CacaologinApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CacaologinApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/cacaologin/CacaologinApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.cacaologin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CacaologinApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
